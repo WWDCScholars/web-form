1b70e2c167e54d9586b1567875ec2b65712a8481
matthijs2704 <matthijs.logemann@xs4all.nl>
2017-04-16 15:45:37 +0200

    Basic vapor setup for the form

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,11 +3,11 @@ import Vapor
 let drop = Droplet()
 
 drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+    return try drop.view.make("index.html")
 }
 
-drop.resource("posts", PostController())
+drop.post { req in
+    return "Data posted: \(req.formURLEncoded.debugDescription)"
+}
 
 drop.run()
