cdde84286d2c930414324f09ca322cceaf85249f
Moritz Sternemann <me@moritzsternemann.de>
2017-04-20 15:46:38 +0200

    Changed input placeholders

--- Frontend/app/components/steps/Step-Form.vue
+++ Frontend/app/components/steps/Step-Form.vue
@@ -7,27 +7,30 @@
       // *TYPE radio
       .form-select(v-if="field.type === 'radio'")
         .form-option(v-for="option in field.options")
-          input(type="radio", :name="field.name", :value="option", :id="option", :required="field.required")
+          input(type="radio", :name="field.name", :value="option", :id="option", :required="field.required", v-model="field.model", @click="evaluateCompletion")
           label(v-bind:for="option") {{ option }}
 
       // *TYPE file
-      .form-file(v-else-if="field.type === 'file'")
-        vue-clip(:options="options")
-          template(slot="clip-uploader-action")
-            div
-              .dz-message.form-file-placeholder: .form-file-icon
-
-          template(slot="clip-uploader-body", scope="props")
-            div(v-for="file in props.files").form-file-preview
-              img(:src="file.dataUrl", :alt="file.name")
-              .file-progress
-                .file-progress-inner
-                  .file-progress-circle.file-progress-left
-                  .file-progress-circle.file-progress-right
+      .form-file(v-else-if="field.type === 'file')
+      //- .form-file(v-else-if="field.type === 'file'")
+      //-   vue-clip(:options="options")
+      //-     template(slot="clip-uploader-action")
+      //-       div
+      //-         .dz-message.form-file-placeholder: .form-file-icon
+      //-
+      //-     template(slot="clip-uploader-body", scope="props")
+      //-       div(v-for="file in props.files").form-file-preview
+      //-         img(:src="file.dataUrl", :alt="file.name")
+      //-         .file-progress
+      //-           .file-progress-inner
+      //-             .file-progress-circle.file-progress-left
+      //-             .file-progress-circle.file-progress-right
 
 
       // *TYPE all
-      input(v-else, :type="field.type", :name="field.name", :placeholder="field.placeholder").form-input
+      .form-input(v-else-if="field.type === 'text'")
+        input(type="text", :name="field.name", :id="field.name", :required="field.required" @focusout="onFocusOut", v-model="field.model", @keyup="evaluateCompletion")
+        label(:for="field.name").form-title {{ field.placeholder }}
 
       .form-comment(v-if="field.comment") {{ field.comment }}
 
@@ -65,6 +68,31 @@ export default {
   },
   mounted() {},
   methods: {
+    evaluateCompletion () {
+      var completed = true
+      for (var g = 0; g < this.step.groups.length; g++) {
+        const group = this.step.groups[g]
+        for (var f = 0; f < group.fields.length; f++) {
+          const field = group.fields[f]
+          if (field.required != false) {
+            if (!field.model) {
+              completed = false
+            }
+          }
+        }
+      }
+
+      console.log(completed)
+    },
+    onFocusOut (event) {
+      const src = event.srcElement
+      if (src.value != '') {
+        src.nextSibling.className = 'form-title input-has-value'
+      } else {
+        src.nextSibling.className = 'form-title'
+      }
+    },
+
     nextStep () {
 
     },
