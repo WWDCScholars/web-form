ba2a60a95ace5ce6af8268d78976b75524a5bb5d
matthijs2704 <matthijs.logemann@xs4all.nl>
2017-04-21 15:04:45 +0200

    Done some work on scholar receiving on login

--- Frontend/app/auth/index.js
+++ Frontend/app/auth/index.js
@@ -1,67 +1,122 @@
 import CloudKit from '../libraries/cloudkit'
 
 const auth = {
-  router: {},
-  vm: undefined,
+    router: {},
+    vm: undefined,
 
-  setVm (vm) {
-    this.vm = vm
-  },
+    setVm(vm) {
+        this.vm = vm
+    },
 
-  user: {},
+    user: {},
 
-  // CloudKit stuff
-  ck: {},
-  _ckConfigureCloudKit () {
-    CloudKit.configure({
-      containers: [{
-        containerIdentifier: 'iCloud.de.moritzsternemann.TIL',
-        apiTokenAuth: {
-          apiToken: 'd9d92006d77e1e706b833d78d371f4c1815f59027d7b1f46edebfc0e78b8a751',
-          persist: true
-        },
-        environment: 'development'
-      }]
-    })
+    // CloudKit stuff
+    ck: {},
+    _ckConfigureCloudKit() {
+        CloudKit.configure({
+            containers: [{
+                containerIdentifier: 'iCloud.com.wwdcscholars.WWDCScholars',
+                apiTokenAuth: {
+                    apiToken: '953f4dbcd6285a925e8224506af8ceebf1436360bd3b025272a2f732370c19c8',
+                    persist: true
+                },
+                environment: 'development'
+            }]
+        })
 
-    this.ck.container = CloudKit.getDefaultContainer()
-    this._ckSetupAuth()
-  },
-  _ckSetupAuth () {
-    this.ck.container.setUpAuth().then((userIdentity) => {
-      // console.log('setUpAuth')
-      if (userIdentity) {
-        this.user = userIdentity
-        this._ckGotoAuthenticatedState(userIdentity)
-      } else {
-        this._ckGotoUnauthenticatedState()
-      }
-    })
-  },
-  _ckGotoAuthenticatedState (userIdentity) {
-    console.log('gotoAuthenticatedState', this)
-    this.user.isAuthenticated = true
+        this.ck.container = CloudKit.getDefaultContainer()
+        this._ckSetupAuth()
+    },
+    _ckSetupAuth() {
+        this.ck.container.setUpAuth().then((userIdentity) => {
+            // console.log('setUpAuth')
+            if (userIdentity) {
+                this.user = userIdentity
+                this._ckGotoAuthenticatedState(userIdentity)
+            } else {
+                this._ckGotoUnauthenticatedState()
+            }
+        })
+    },
+    _ckGotoAuthenticatedState(userIdentity) {
+        console.log('gotoAuthenticatedState', this)
+        this.user.isAuthenticated = true
 
-    this.ck.container
-      .whenUserSignsOut()
-      .then(this._ckGotoUnauthenticatedState.bind(this))
+        this.ck.container
+            .whenUserSignsOut()
+            .then(this._ckGotoUnauthenticatedState.bind(this))
 
-    this.router.replace({ name: 'welcome' })
-  },
-  _ckGotoUnauthenticatedState(error) {
-    // console.log('gotoUnauthenticatedState', this)
+        var database = this.ck.container.getDatabaseWithDatabaseScope(
+            CloudKit.DatabaseScope["PUBLIC"]
+        );
 
-    if (error && error.ckErrorCode === 'AUTH_PERSIST_ERROR') {
-      console.error('ERROR', error)
+        database.fetchRecords(userIdentity.userRecordName)
+            .then(function(response) {
+                if (response.hasErrors) {
+
+                    // Handle the errors in your app.
+                    throw response.errors[0];
+
+                } else {
+                    var record = response.records[0];
+                    var scholarReference = record.fields.scholar;
+
+                    if (scholarReference === undefined) {
+                        // Scholar isn't linked yet or doesn't exist at all
+
+                        this.router.replace({
+                            name: 'welcome'
+                        });
+
+                    } else {
+                        // Scholar already exising
+
+                        database.fetchRecords(scholarReference.value.recordName)
+                            .then(function(response) {
+                                if (response.hasErrors) {
+
+                                    // Handle the errors in your app.
+                                    throw response.errors[0];
+
+                                } else {
+                                    var scholar = response.records[0];
+                                    var wwdcYears = scholar.fields.wwdcYears.value
+
+                                    for (var i = 0; i < wwdcYears.length; i++) {
+                                        var obj = wwdcYears[i];
+
+                                        if (obj.recordName === 'WWDC 2017') {
+                                        console.log("Contains WWDC 2017, to thankyou");
+                                            auth.router.push({ name: 'thankyou' })
+                                            return;
+                                        }
+                                    }
+                                }
+                            });
+
+                    }
+                    // Render the fetched record.
+                    console.log("Hi!");
+                    console.log(record);
+                }
+            });
+    },
+    _ckGotoUnauthenticatedState(error) {
+        // console.log('gotoUnauthenticatedState', this)
+
+        if (error && error.ckErrorCode === 'AUTH_PERSIST_ERROR') {
+            console.error('ERROR', error)
+        }
+
+        this.ck.container
+            .whenUserSignsIn()
+            .then(this._ckGotoAuthenticatedState.bind(this))
+            .catch(this._ckGotoUnauthenticatedState.bind(this))
+
+        this.router.replace({
+            name: 'signin'
+        })
     }
-
-    this.ck.container
-      .whenUserSignsIn()
-      .then(this._ckGotoAuthenticatedState.bind(this))
-      .catch(this._ckGotoUnauthenticatedState.bind(this))
-
-    this.router.replace({ name: 'signin' })
-  }
 }
 
 export default auth
