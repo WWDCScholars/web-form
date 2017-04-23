aa8bd57e5e7239814b6e36c0facdeaf3b42be8b9
Moritz Sternemann <me@moritzsternemann.de>
2017-04-23 17:36:37 +0200

    Implemented linking using ck module methods

--- Frontend/app/components/Link.vue
+++ Frontend/app/components/Link.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
 .container.container-outer.color-gray
+  modal(v-if="linkInProgress")
+    h3(slot="header").color-blue Looking for your account...
+    div(slot="body").spinner.spinner-blue.modal-spinner
+
   h2.color-blue Welcome to WWDCScholars!
   h3.color-blue.link-header Have you signed up for an account before?
 
@@ -15,7 +19,7 @@
         label(for="email").form-title Email
       .form-comment {{ errorComment }}
 
-    button(@click="link").form-cta.form-cta-primary.form-cta-right Link
+    button(@click="link", :disabled="linkInProgress").form-cta.form-cta-primary.form-cta-right Link
 </template>
 
 <script>
@@ -25,7 +29,9 @@ export default {
   data () {
     return {
       hasSignedUpBefore: false,
-      errorComment: ''
+      errorComment: '',
+
+      linkInProgress: false
     }
   },
   computed: {},
@@ -45,128 +51,29 @@ export default {
     signedUpBefore () {
       this.hasSignedUpBefore = true
     },
-    link () {
+    async link () { // TODO: Spinner
+      if (this.linkInProgress) {
+        return
+      }
+
       let emailValue = this.$refs.signedUpBefore_email.value
       if (emailValue.length < 4) {
         return
       }
 
-      let database = this.auth.ck.container.getDatabaseWithDatabaseScope(
-        CloudKit.DatabaseScope['PUBLIC']
-      )
-      let self = this
-      database.performQuery({
-        recordType: 'Scholar',
-        filterBy: [{
-          comparator: 'EQUALS',
-          fieldName: 'email',
-          fieldValue: {value: emailValue}
-        }]
-      }, {
-        desiredKeys: ['recordName', 'recordChangeTag'],
-        resultsLimit: 1
-      })
-      // database.fetchRecords(emailValue)
-        .then(function (response) {
-          if (response.hasErrors || response.records.length != 1) {
-            self.errorComment = 'Your email address could not be found. Are you sure it is the one you used in the past?'
-          } else {
-            let scholar = response.records[0];
-            self.auth.scholar = scholar
-            let userIdentity = self.auth.user
-
-            database.fetchRecords(userIdentity.userRecordName)
-              .then(function (response) {
-                if (response.hasErrors) {
-
-                  // Handle the errors in your app.
-                  throw response.errors[0];
-                } else {
-                  let record = response.records[0];
-                  let fields = {
-                    scholar: { recordName: scholar.recordName, action: 'NONE' }
-                  }
-
-                  self.auth._ckSaveRecords('PUBLIC', record.recordName, record.recordChangeTag, 'Users', null, null, null, null, null, null, null, fields, null, (errors, response, zoneID, databaseScope) => {
-                    if (errors) {
-                      console.error(errors[0])
-                      self.$router.push({ name: 'error' })
-                      return
-                    }
-
-                    // Linking successful
-                    database.fetchRecords(scholar.recordName)
-                    .then(function(response) {
-                      if (response.hasErrors) {
-                        console.error(response.errors[0])
-                        self.$router.push({ name: 'error' })
-
-                      } else {
-                        var scholar = response.records[0];
-                        var wwdcYears = scholar.fields.wwdcYears.value
-                        var socialMediaRef = scholar.fields.socialMedia
-
-                        for (var i = 0; i < wwdcYears.length; i++) {
-                          var obj = wwdcYears[i];
-
-                          if (obj.recordName === 'WWDC 2017') {
-                            console.log("Contains WWDC 2017, to thankyou");
-                            self.$router.push({ name: 'thankyou' })
-                            return;
-                          }
-                        }
-
-                        console.log("Scholar exists but hasn't filled form");
-                        console.log("Fetching social media");
-                        database.fetchRecords(socialMediaRef.value.recordName)
-                        .then(function(socialResponse) {
-                          if (socialResponse.hasErrors) {
-                            console.error(response.errors[0])
-                            self.$router.push({ name: 'error' })
-
-                          } else {
-                            var socialMedia = socialResponse.records[0];
-                            self.auth.scholarSocialMedia = socialMedia
-                            console.log(socialMedia);
-                            var steps = self.auth.vm.$store.steps
-                            for (var s = 0; s < steps.length; s++) {
-                              const step = steps[s]
-                              for (var g = 0; g < step.groups.length; g++) {
-                                const group = step.groups[g]
-                                for (var f = 0; f < group.fields.length; f++) {
-                                  const field = group.fields[f]
-                                  if (scholar.fields[field.name]) {
-                                    if (field.type === 'location') {
-                                      let lf = scholar.fields[field.name].value
-                                      field.model = lf.latitude + ',' + lf.longitude
-                                    } else {
-                                      field.model = scholar.fields[field.name].value
-                                    }
-                                  } else if (socialMedia.fields[field.name]) {
-                                    console.log(field.name);
-                                    field.model = socialMedia.fields[field.name].value
-                                  }
-                                }
-                              }
-                            }
-
-                            self.$router.push({ name: 'welcome' })
-                            // self.$router.push({ name: 'step', params: { step: '2-application' } })
-                            // self.auth.router.replace({ name: 'welcome' });
-                          }
-                        });
-                      }
-                    });
-
-                    //self.$router.push({ name: 'welcome' })
-                  })
-                }
-              });
-          }
-        });
+      try {
+        this.linkInProgress = true
+        await this.auth.linkScholarByEmail(emailValue)
+        this.linkInProgress = false
+      } catch (error) {
+        console.error(error)
+        this.errorComment = 'Your email address could not be found. Are you sure it is the one you used in the past?'
+      }
     }
   },
-  components: {}
+  components: {
+    'modal': require('./Modal.vue')
+  }
 }
 </script>
 
