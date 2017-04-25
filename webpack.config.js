685ea111f26cbbabe6f9b4277e5f88ad2541962c
Moritz Sternemann <me@moritzsternemann.de>
2017-04-25 16:12:39 +0200

    Temporarily enabled sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -96,7 +96,7 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = false
+  // module.exports.devtool = false
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: '"production"' }
