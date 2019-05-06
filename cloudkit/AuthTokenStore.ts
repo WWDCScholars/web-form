de1b22aaab86b8deb0ad7b941164602448096330
Moritz Sternemann <moritz@strnmn.me>
2019-05-06 21:53:43 +0200

    fix: Return null instead of undefined in AuthTokenStore

--- cloudkit/AuthTokenStore.ts
+++ cloudkit/AuthTokenStore.ts
@@ -1,27 +1,29 @@
 export default class AuthTokenStore {
-  putToken(containerIdentifier: string, authToken: string | undefined): void {
+  putToken(containerIdentifier: string, authToken: string | null): void {
     const date = new Date()
+    let path = ''
 
     if (authToken) {
       // set time to expire in 14 days
       date.setTime(date.getTime() + (14 * 24 * 60 * 60 * 1000))
+      path = '; path=/'
     } else {
       // set time to 1970-01-01
       date.setTime(0)
     }
 
     // set the cookie
-    document.cookie = `${containerIdentifier}=${authToken || ''}; expires=${date.toUTCString()}; path=/`
+    document.cookie = `${containerIdentifier}=${authToken || ''}; expires=${date.toUTCString()}${path}`
   }
 
-  getToken(containerIdentifier: string): string | undefined {
+  getToken(containerIdentifier: string): string | null {
     const value = '; ' + document.cookie
     const parts = value.split('; ' + containerIdentifier + '=')
 
     if (parts.length === 2) {
-      return parts.pop()!.split(';').shift()
+      return parts.pop()!.split(';').shift() || null
     }
 
-    return undefined
+    return null
   }
 }
