065250bc68b94378e3e2f8bafe5fffd3d2a8e88e
Moritz Sternemann <moritz@wwdcscholars.com>
2021-06-02 21:46:40 +0200

    feat: Add Instagram and Apple Developer Profile fields

--- model/ScholarSocialMedia.ts
+++ model/ScholarSocialMedia.ts
@@ -2,12 +2,13 @@ import { CloudKit, Record, Field } from '.'
 
 export default class ScholarSocialMedia extends Record {
   static recordType = 'ScholarSocialMedia'
-  
+
   @Field public scholar!: CloudKit.Reference
   @Field public discord!: string
   @Field public facebook!: string
   @Field public github!: string
   @Field public imessage!: string
+  @Field public instagram!: string
   @Field public itunes!: string
   @Field public linkedin!: string
   @Field public twitter!: string
